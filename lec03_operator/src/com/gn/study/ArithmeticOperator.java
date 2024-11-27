5889770e36d8e2b6a8ba1b31086717fac77e048b
devsj24 <ssj2249@naver.com>
2024-11-27 15:29:05 +0900

    [ssj] Operator
    	- 산술연산자
    	- 복합 대입 연산자

--- lec03_operator/src/com/gn/study/ArithmeticOperator.java
+++ lec03_operator/src/com/gn/study/ArithmeticOperator.java
@@ -2,6 +2,7 @@ package com.gn.study;
 
 public class ArithmeticOperator {
 	public static void main(String[] args) {
+//		1. 산술 연산자
 		int su1 = 10;
 		int su2 = 3;
 		
@@ -10,6 +11,25 @@ public class ArithmeticOperator {
 		System.out.println(su1 * su2); // 30
 		System.out.println(su1 / su2); // 3
 		System.out.println(su1 % su2); // 1
+		
+//		2. 복합 대입 연산자
+		int a = 5;
+//		a = a + 3; 
+		a += 3;
+		System.out.println(a); // 8
+		
+		int b = 10;
+		b -= 4; // 6 
+		b *= 4;
+		System.out.println(b); // 24
+		
+		int c = 10;
+		c /= 3; // 3
+		System.out.println(c);
+		
+		int d = 10;
+		d %= 3; // 1
+		System.out.println(d);
 	}
 
 }
