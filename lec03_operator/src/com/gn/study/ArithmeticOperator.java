91c2763dba0e00e9d7572ed819990d122a740fde
devsj24 <ssj2249@naver.com>
2024-11-27 15:59:27 +0900

    [ssj] 증감 연산자

--- lec03_operator/src/com/gn/study/ArithmeticOperator.java
+++ lec03_operator/src/com/gn/study/ArithmeticOperator.java
@@ -30,6 +30,20 @@ public class ArithmeticOperator {
 		int d = 10;
 		d %= 3; // 1
 		System.out.println(d);
+		
+//		3. 증감 연산자
+//		(1) 전위 증감 연산자
+		int pre = 5;
+		System.out.println(pre); // 5
+		System.out.println(++pre); // 6
+		
+//		(2) 후위 증감 연산자
+		int post = 5;
+		System.out.println(post); // 5
+		System.out.println(post++); // 5
+		System.out.println(post); // 6
+		
+		System.out.println("");
 	}
 
 }
